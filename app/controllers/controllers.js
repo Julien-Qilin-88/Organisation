9452c826937012decb08474b7c3af27dd68e0683
Julien-Qilin-88 <112284313+juju88dev@users.noreply.github.com>
2023-07-25 14:59:42 +0200

    fonction to_char pour gestion affichage des date et heure

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -4,10 +4,11 @@ import database from '../database.js';
 export const accueil = async (req, res) => {
 
     if (req.session.user) {
+
         const result = await database.query('SELECT * FROM "note" WHERE id_note = $1', [req.session.user.id]);
         const notes = result.rows;
-
-        const rdv = await database.query('SELECT * FROM "rdv" WHERE id_rdv = $1', [req.session.user.id]);
+      
+        const rdv = await database.query('SELECT nom, lieu, TO_CHAR("date", \'DD/MM/YYYY\') AS "date", TO_CHAR("heure", \'HH24:MI\') AS "heure" FROM "rdv" WHERE id_rdv = $1', [req.session.user.id]);
         const rdvs = rdv.rows;
 
         const favori = await database.query('SELECT * FROM "favori" WHERE id_favori = $1', [req.session.user.id]);
@@ -16,9 +17,7 @@ export const accueil = async (req, res) => {
         res.render('accueil', { user: req.session.user, notes, rdvs, favoris });
     } else {
         res.render('accueil');
-
     }
-
 }
 
 export const kanban = (req, res) => {
