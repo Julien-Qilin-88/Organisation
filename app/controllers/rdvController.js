cf418e6bf1a489c61d3caaf27abc98c6bb4b9b9b
Julien <pierratlabollejulien@gmail.com>
2023-08-02 14:30:23 +0200

    fix message error

--- app/controllers/rdvController.js
+++ app/controllers/rdvController.js
@@ -1,7 +1,7 @@
 import validator from 'validator';
 import database from '../database.js';
 import slugify from 'slugify';
-
+import Rdv from '../models/rdv.js';
 const rdvController = {
     // page ajout note
     addRdvPage: (req, res) => {
@@ -14,18 +14,16 @@ const rdvController = {
     addRdvAction: async (req, res) => {
 
         try {
-     
-            const nom = req.body.nom;
-            const lieu = req.body.lieu;
-            const date = req.body.date;
-            const heure = req.body.heure;
-            
-            // retirer les : de l'heure
-            const heureSansDeuxPoints = heure.replace(':', '');
 
-            const id = req.session.user.id;
+            const rdv = new Rdv(
+                req.body.nom,
+                req.body.lieu,
+                req.body.date,
+                req.body.heure,
+                req.session.user.id
+            );
 
-            await database.query('INSERT INTO "rdv" (nom, lieu, date, heure, id_rdv) VALUES ($1, $2, $3, $4, $5)', [nom, lieu, date, heureSansDeuxPoints, id]);
+            await rdv.create();
 
             res.redirect('/');
 
@@ -40,7 +38,7 @@ const rdvController = {
             const favori = await database.query('SELECT * FROM "favori" WHERE id_favori = $1', [req.session.user.id]);
             const favoris = favori.rows;
     
-            res.render('accueil', { user: req.session.user, notes, rdvs, favoris, error: error.message });
+            res.render('accueil', { user: req.session.user, notes, rdvs, favoris, errorRdv: error.message });
                 // avec gestion des erreurs et ajout des select de la page d'accueil qui sont dans la fonction accueil du fichier controllers.js
                 
             }
---
+++ app/models/rdv.js
@@ -0,0 +1,108 @@
+import validator from 'validator';
+import client from '../database.js';
+
+class rdv {
+    #nom;
+    #lieu;
+    #date;
+    #heure;
+    #id_rdv;
+
+    constructor(nom, lieu, date, heure, id_rdv) {
+        this.nom = nom;
+        this.lieu = lieu;
+        this.date = date;
+        this.heure = heure;
+        this.id_rdv = id_rdv;
+    }
+
+    get nom() {
+        return this.#nom;
+    }
+
+    get lieu() {
+        return this.#lieu;
+    }
+
+    get date() {
+        return this.#date;
+    }
+
+    get heure() {
+        return this.#heure;
+    }
+
+    get id_rdv() {
+        return this.#id_rdv;
+    }
+
+    set nom(value) {
+        if (!value || typeof value !== 'string') {
+            throw new Error('Nom obligatoire');
+        }
+        else if (!validator.isLength(value, { min: 1, max: 15 })) {
+            throw new Error('Le nom doit contenir entre 1 et 15 caractères');
+        }
+        this.#nom = value;
+    }
+
+    set lieu(value) {
+        if (!value || typeof value !== 'string') {
+            throw new Error('Lieu obligatoire');
+        }
+        else if (!validator.isLength(value, { min: 1, max: 15 })) {
+            throw new Error('Le lieu doit contenir entre 1 et 15 caractères');
+        }
+        this.#lieu = value;
+    }
+
+    set date(value) {
+        if (!value || typeof value !== 'string') {
+            throw new Error('Date obligatoire');
+        }
+    //    date en francais DD/MM/YYYY
+        else if (!validator.isDate(value, 'DD/MM/YYYY')) {
+            throw new Error('La date doit être au format DD/MM/YYYY');
+        }
+
+
+
+        this.#date = value;
+    }
+
+    set heure(value) {
+        const heureSansDeuxPoints = value.replace(':', '');
+
+        if (!value || typeof value !== 'string') {
+            throw new Error('Heure obligatoire');
+        }
+        // l'heure doit etre 13:30 avec les :
+        else if (!validator.isAfter(value, '00:00') || !validator.isBefore(value, '23:59')) {
+            throw new Error('L\'heure doit être comprise entre 00:00 et 23:59 et au format HH:MM');
+        }
+        this.#heure = value;
+    }
+
+    set id_rdv(value) {
+        if (!value || typeof value !== 'number') {
+            throw new Error('Id obligatoire');
+        }
+        this.#id_rdv = value;
+    }
+
+    async create() {
+        try {
+            await client.query({
+                text: 'INSERT INTO rdv (nom, lieu, date, heure, id_rdv) VALUES ($1, $2, $3, $4, $5)',
+                values: [this.nom, this.lieu, this.date, this.heure, this.id_rdv]
+               
+            });
+        } catch (error) {
+            throw new Error(error.message);
+        }
+
+}
+
+}
+
+export default rdv;
